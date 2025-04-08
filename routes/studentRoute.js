327b0d22dbe49b212fd401afa9370a694c6731c3
Partha Maity <parthamaity2004@gmail.com>
2025-04-08 21:11:10 +0530

    Upload the image into a folder using postman

--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const Student=require('./../models/student'); 
+const multer=require('multer');
 
+
+// set up multer to store files in upload folders
+const storage= multer.diskStorage({
+    destination: (req,file, cb) =>{ //cb is callback
+        cb(null,'uploads/'); // add the image into this uploads folder
+    },
+    filename: (req, file,cb)=>{
+        const suffix=Date.now(); // this is add bec same rename image problemmm..so we add a date then date is not same  
+        cb(null,suffix + '-' + file.originalname);
+    }
+});
+
+const upload= multer({storage});
 
 // Route to create a new student
-router.post('/create', async(req,res) =>{
+router.post('/create',upload.single('photo'), async(req,res) =>{ // upload.single('photo'), this is middleware
     try{
         const {name, age, email, phone , address}= req.body;
 
